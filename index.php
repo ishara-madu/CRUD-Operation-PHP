184c769675fe977f16d6f06c7125cdbe4d640d2d
ishara madushanka <isharamadushankab@gmail.com>
2024-05-31 10:20:35 +0530

    single coat changed

--- index.php
+++ index.php
@@ -1,67 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>crud</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <?php
-        include 'db.con.php';
+    include 'db.con.php';
     ?>
     <form method="post">
         <div class="btn">
             <input type="number" name="id" id="" placeholder="Enter ID">
-        <input type="text" name="name" id="" placeholder="Enter Name">
+            <input type="text" name="name" id="" placeholder="Enter Name">
         </div>
-        
+
         <div class="btn">
             <input type="submit" name="insert" value="Insert">
-        <input type="submit" name="update" value="Update">
-        <input type="submit" name="delete" value="Delete">
+            <input type="submit" name="update" value="Update">
+            <input type="submit" name="delete" value="Delete">
         </div>
-        
+
 
         <table>
             <thead>
                 <tr>
-                    <th>ID</th><th>Name</th>
+                    <th>ID</th>
+                    <th>Name</th>
                 </tr>
             </thead>
             <tbody>
                 <?php
-                    $sql = "SELECT * FROM `user`";
-                    $result = mysqli_query($conn, $sql);
-                    while($row = mysqli_fetch_assoc($result)){
-                        echo "<tr><td>".$row['id']."</td><td>".$row['name']."</td></tr>";
-                    }
-               ?>
+                $sql = "SELECT * FROM `user`";
+                $result = mysqli_query($conn, $sql);
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr><td>" . $row['id'] . "</td><td>" . $row['name'] . "</td></tr>";
+                }
+                ?>
             </tbody>
         </table>
     </form>
     <?php
-    if(isset($_POST['insert'])){
+    if (isset($_POST['insert'])) {
         $id = $_POST['id'];
         $name = $_POST['name'];
-        $sql = "INSERT INTO `user` (`id`, `name`) VALUES ('$id', '$name')";
+        $sql = "INSERT INTO `user` (id, name) VALUES ('$id', '$name')";
         mysqli_query($conn, $sql);
         header("Refresh:0");
     }
-    if(isset($_POST['update'])){
+    if (isset($_POST['update'])) {
         $id = $_POST['id'];
         $name = $_POST['name'];
-        $sql = "UPDATE `user` SET `name` = '$name' WHERE `user`.`id` = '$id'";
+        $sql = "UPDATE user SET name = '$name' WHERE id = '$id'";
         mysqli_query($conn, $sql);
         header("Refresh:0");
     }
-    if(isset($_POST['delete'])){
+    if (isset($_POST['delete'])) {
         $id = $_POST['id'];
-        $sql = "DELETE FROM `user` WHERE `user`.`id` = '$id'";
+        $sql = "DELETE FROM user WHERE id = '$id'";
         mysqli_query($conn, $sql);
         header("Refresh:0");
     }
 
     ?>
 </body>
+
 </html>
